e20bf61538c1cba9d51c576e28897c6bb91ff13e
Brett <brett_matsumoto@yahoo.com>
2019-04-01 19:18:54 -1000

    completed vault work

--- vault.js
+++ vault.js
@@ -1,4 +1,25 @@
 'use strict';
 module.exports = function() {
-  
-};
+  let myVault = {};
+
+  const setValue = function(key,value){
+    if (!key || !value){
+      return null;
+    } else {
+      myVault.key = value;
+    }
+  }
+
+  const getValue = function (key){
+    if (myVault.key){
+      return myVault.key;
+    } else {
+      return null;
+    }
+  }
+
+  return {
+    setValue: setValue,
+    getValue: getValue
+  }
+}
